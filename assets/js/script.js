6b2f277fe5f674481ba26faaa178c6596e811ac8
Maverick <Mav33987@gmail.com>
2021-12-21 23:39:40 -0600

    cleaned up data from api using slice and split in the console and created a graph using chart.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,47 @@
-var graph = document.querySelector('#graph');
 
+const xlabels = [];
+const yconfirmed = [];
 
-fetch('https://covid19.richdataservices.com/rds/api/query/int/jhu_country/select?cols=date_stamp,cnt_confirmed,cnt_death,cnt_recovered&where=(iso3166_1=US)&format=amcharts&limit=5000')
-.then(data =>{
+async function getData() {
+    var response = await fetch('https://covid19.richdataservices.com/rds/api/query/int/jhu_country/select?cols=date_stamp,cnt_confirmed,cnt_death,cnt_recovered&where=(iso3166_1=US)&limit=100');
+    var data = await response.text();
+    console.log(data);
 
-    var graphEl = data['dataProvider'];
-
-    graph.innerHTML = graphEl;
+    var table = data.split('], [').slice(1);
+    table.forEach (row => {
+        var columns = row.split(',');
+        var year = columns[0];
+        xlabels.push(year);
+        var confirmed = columns[1];
+        yconfirmed.push(confirmed);
+        console.log(year, confirmed);
+    });
     
-})
-.catch(err => console.log(err));
+}
 
-console.log('https://covid19.richdataservices.com/rds/api/query/int/jhu_country/select?cols=date_stamp,cnt_confirmed,cnt_death,cnt_recovered&where=(iso3166_1=US)&format=amcharts&limit=5000')
+async function chartIt () {
+await getData();
+const ctx = document.getElementById('chart').getContext('2d');
 
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: xlabels,
+        datasets: [{
+            label: 'Confirmed COVID cases in the US',
+            data: yconfirmed,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+   
+});
+}
 
+chartIt();
+getData();
